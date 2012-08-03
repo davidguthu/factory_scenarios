a35ee04abc56da885b7454336465a56dac0bacff
Jason Fleetwood-Boldt <jason@datatravels.com>
2012-08-03 13:56:12 -0400

    fixing paths style for rails 3.1+ support

--- lib/factory_scenarios/engine.rb
+++ lib/factory_scenarios/engine.rb
@@ -10,15 +10,14 @@ module FactoryScenarios
     end
   end
   
-  class Engine < Rails::Engine
-    
+  class Engine < Rails::Engine    
     initializer "paths" do
-      paths.factories Rails.root + "spec/factories"
-      paths.factory_scenario_datastore Rails.root + "db/factory_scenarios.#{Rails.env}.yml"
-
+      paths['factories'] = Rails.root + "spec/factories"
+      paths['factory_scenario_datastore'] = Rails.root + "db/factory_scenarios.#{Rails.env}.yml"
+      
       config.factory_scenarios_moneta_backend = :YAML unless config.respond_to? :factory_scenarios_moneta_backend
       config.factory_scenarios_moneta_config = {
-        :path => config.paths.factory_scenario_datastore.first
+        :path => config.paths['factory_scenario_datastore']
       } unless config.respond_to? :factory_scenarios_moneta_config
     end
 
@@ -27,7 +26,7 @@ module FactoryScenarios
       # FactoryGirl.factories = {}
       
       root = Rails.application.config.root
-      factories_path = FactoryScenarios::Engine.paths.factories.first
+      factories_path = FactoryScenarios::Engine.paths['factories'].first
       globstring = (factories_path + "/**/*.rb").to_s
 
       
