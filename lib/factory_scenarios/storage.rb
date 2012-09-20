710de160bb755b15ad02d576b2f67482abbe72c5
David Guthu <david@chloeandisabel.com>
2012-09-19 23:23:13 -0400

    fixed dev bug

--- lib/factory_scenarios/storage.rb
+++ lib/factory_scenarios/storage.rb
@@ -1,5 +1,5 @@
 module FactoryScenarios
   require "moneta/adapters/#{FactoryScenarios::Engine.config.factory_scenarios_moneta_backend.to_s.downcase}"
   storage_class = Moneta::Adapters.const_get(FactoryScenarios::Engine.config.factory_scenarios_moneta_backend)
-  Storage = storage_class.new :path => "db/factory_scenarios.development.yml"
+  Storage = storage_class.new FactoryScenarios::Engine.config.factory_scenarios_moneta_config
 end
